9c2be27094ba4365b0d9e668912948ddc653e6f3
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 21:31:46 +0530

    Add Message class with attributes and constructor

---
+++ Message.java
@@ -0,0 +1,10 @@
+public class Message {
+    // Attributes
+    private int id;
+    private int timestamp;
+
+    public Message(int id, int ts) {
+        this.id = id;
+        this.timestamp = ts;
+    }
+}

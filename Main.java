7a0826f6fe23de0d70bf5781817c7addd73ec0b9
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 21:11:36 +0530

    Refactor main method to initialize Node instances and set connections

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+        Node n4 = new Node(4);
+
+        List<Node> arr = Arrays.asList(n1, n2, n3, n4); // creating an array from individual elements
+        n1.setOtherNodes(arr);
+        n2.setOtherNodes(arr);
+        n3.setOtherNodes(arr);
+        n4.setOtherNodes(arr);
     }
 }

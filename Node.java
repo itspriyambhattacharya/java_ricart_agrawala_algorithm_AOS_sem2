fd0db7063410d8cd0c44214cba43581f614be7e8
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 20:59:37 +0530

    Add otherNodes attribute to Node class and initialize in constructor

--- Node.java
+++ Node.java
@@ -1,12 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Node {
     // Attributes
     private int id;
     private boolean inCS;
     private boolean reqCS;
+    private List<Node> otherNodes;
 
+    // Constructor
     public Node(int id) {
         this.id = id;
         this.inCS = false;
         this.reqCS = false;
+        this.otherNodes = new ArrayList<>();
     }
+
+    // Methods
 }

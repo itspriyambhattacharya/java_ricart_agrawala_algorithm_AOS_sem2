ad5ddb3ce860741900d6f645b6bfe78adc07cdf9
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 20:57:28 +0530

    Implement Node class with attributes and constructor

---
+++ Node.java
@@ -0,0 +1,12 @@
+public class Node {
+    // Attributes
+    private int id;
+    private boolean inCS;
+    private boolean reqCS;
+
+    public Node(int id) {
+        this.id = id;
+        this.inCS = false;
+        this.reqCS = false;
+    }
+}

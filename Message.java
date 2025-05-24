773163e55941924aa8c059343fe2a55080f93e5a
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-24 23:00:21 +0530

    Add getter methods for id and timestamp attributes in Message class

--- Message.java
+++ Message.java
@@ -3,6 +3,14 @@ public class Message {
     private int id;
     private int timestamp;
 
+    public int getTimestamp() {
+        return this.timestamp;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
     public Message(int id, int ts) {
         this.id = id;
         this.timestamp = ts;

30c253cf9a9de3ee4eb1c5e8f71b53b0d66949f9
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-05-28 07:15:11 +0530

    Refactor main method to dynamically create process nodes based on user input

--- Main.java
+++ Main.java
@@ -1,25 +1,30 @@
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-        Node n5 = new Node(5);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("\nEnter Number of Processes:\t");
+        int n = Integer.parseInt(br.readLine());
 
-        List<Node> arr = Arrays.asList(n1, n2, n3, n4, n5); // creating an array from individual elements
-        n1.setOtherNodes(arr);
-        n2.setOtherNodes(arr);
-        n3.setOtherNodes(arr);
-        n4.setOtherNodes(arr);
-        n5.setOtherNodes(arr);
+        List<Node> processes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            Node newNode = new Node(i + 1);
+            processes.add(newNode); // adding nodes to the ArrayList
+        }
 
-        n1.start();
-        n2.start();
-        n3.start();
-        n4.start();
-        n5.start();
+        for (Node node : processes) {
+            node.setOtherNodes(processes);
+        }
+
+        for (Node node : processes) {
+            node.start();
+        }
+
+        br.close();
+
     }
 }
